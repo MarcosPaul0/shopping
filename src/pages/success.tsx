f7ee7e6bc2298d035ce4b380bc27ac6bdc05e835
MarcosPaul0 <marcosphip7@gmail.com>
2023-03-16 19:42:27 -0300

    feat: adiciona renderização de múltiplas camisas

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -4,17 +4,21 @@ import Head from "next/head";
 import Link from "next/link";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
-import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import {
+  ImageContainer,
+  ImagesContainer,
+  SuccessContainer,
+} from "../styles/pages/success";
 
 interface SuccessProps {
   costumerName: string;
-  product: {
+  products: {
     name: string;
     imageUrl: string;
-  }
+  }[];
 }
 
-export default function Success({ costumerName, product }: SuccessProps) {
+export default function Success({ costumerName, products }: SuccessProps) {
   return (
     <>
       <Head>
@@ -26,48 +30,65 @@ export default function Success({ costumerName, product }: SuccessProps) {
       <SuccessContainer>
         <h1>Compra efetuada</h1>
 
-        <ImageContainer>
-          <Image src={product.imageUrl} width={120} height={110} alt="" />
-        </ImageContainer>
+        <ImagesContainer>
+          {products.map((product) => (
+            <ImageContainer key={product.name}>
+              <Image src={product.imageUrl} width={120} height={110} alt="" />
+            </ImageContainer>
+          ))}
+        </ImagesContainer>
 
         <p>
-          Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+          Uhuul <strong>{costumerName}</strong>
+          {products.length > 1 ? (
+            ", seus produtos já estão"
+          ) : (
+            <span>
+              , sua <strong>{products[0].name}</strong> já está
+            </span>
+          )}{" "}
+          a caminho da sua casa.
         </p>
 
-        <Link href="/">
-          Voltar ao catálogo
-        </Link>
+        <Link href="/">Voltar ao catálogo</Link>
       </SuccessContainer>
     </>
-  )
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   if (!query.session_id) {
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
-      }
-    }
+      },
+    };
   }
 
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
-    expand: ['line_items', 'line_items.data.price.product']
+    expand: ["line_items", "line_items.data.price.product"],
   });
 
   const costumerName = session.customer_details.name;
-  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  const items = session.line_items.data;
+
+  const products = items.map((product) => {
+    const productData: Stripe.Product = product.price.product as Stripe.Product;
+
+    return {
+      name: productData.name,
+      imageUrl: productData.images[0],
+    };
+  });
 
   return {
     props: {
       costumerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0]
-      }
-    }
-  }
-}
+      products,
+    },
+  };
+};
